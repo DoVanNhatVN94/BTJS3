ddfc2ecc17f4cf97b94176e12baeee6f9a7fccce
Nhat <dovannhatdn94@gmail.com>
2021-12-09 22:22:44 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+/**
+ * Xắp xếp thứ tự tăng dần
+ * 
+ * Khối 1: 
+ * số 1 (a)
+ * số 2 (b)
+ * số 3 (c)
+ * 
+ * Khối 2:
+ * b1: tạo biến đệm: x1 x2 x3
+ * b2:Lập điều kiện 
+ *  if (a <= b && b <= c) x1 = a; x2 = b; x3 = c;
+ *  if (a <= b && b >= c) { if (a < c) { x1 = a; x2 = c; x3 = b } else { x1 = c; x2 = a; x3 = b } 
+ *  if (a >= b && b <= c) { if (a <= c) { x1 = b; x2 = a; x3 = c } else { x1 = b; x2 = c; x3 = a } }
+ *   if (a >= b && b >= c) { x1 = c; x2 = b; x3 = a; }
+ *  b3: xuất ra màn hình: document.getElementById("txtContent1").innerHTML = x1 + "<" + x2 + "<" + x3;
+ * Khối 3: 
+ * nhập vào 2 3 1 
+ * => 1<2<3
+ */
+
+
+
 function tangDan() {
     var a = Number(document.getElementById("inpNum1").value);
     var b = Number(document.getElementById("inpNum2").value);
@@ -14,6 +37,26 @@ function tangDan() {
     document.getElementById("txtContent1").innerHTML = x1 + "<" + x2 + "<" + x3;
 }
 
+/**
+ * Bài 2 Chào hỏi
+ * 
+ * khối 1
+ * lấy giá trị từ bộ select (fa)
+ * 
+ * Khối 2
+ * b1: lập điều kiện
+ * if (fa == 1) { t.innerHTML = "Xin chào Bố!" }
+ * if (fa == 2) { t.innerHTML = "Xin chào Mẹ!" }
+ * if (fa == 3) { t.innerHTML = "Xin chào Anh Trai!" }
+ * if (fa == 4) { t.innerHTML = "Xin chào Em gái!" }
+    else { t.innerHTML = "Xin chào Người lạ ơi!" }
+ * b2: xuất ra màn hình document.getElementById("txtFamily").innerHTML ;
+
+ * Khối 3:
+ * click chọn chào mẹ => Xin chào Mẹ!
+ */
+
+
 function chaoHoi() {
     var fa = document.getElementById("inpFamily").value;
     var t = document.getElementById("txtFamily");
@@ -23,6 +66,42 @@ function chaoHoi() {
     else if (fa == 4) { t.innerHTML = "Xin chào Em gái!" }
     else { t.innerHTML = "Xin chào Người lạ ơi!" }
 }
+
+/**
+ * bai 3 Đếm số chẵn lẽ
+ * 
+ * Khối 1:
+ * số 1 (num1)
+ * số 2 (num2)
+ * số 3 (num3)
+ * 
+ * Khối 2:
+ * 
+ * b1: tạo biến đệm: đếm lẽ(countL), đếm chẵn (countC)
+ * b2: Lập điều kiện
+    if (num1 < 0 || num2 < 0 || num3 < 0 || num1 == "" || num2 == "" || num3 == "") {alert("Nhập lại");}
+ *  if (num1 % 2 == 0)
+        countC++;
+ * else
+        countL++;
+    if (num2 % 2 == 0)
+        countC++;
+    else
+        countL++;
+    if (num3 % 2 == 0)
+        countC++;
+    else
+        countL++;
+ * b3: xuất ra màn hình
+        document.getElementById("txtDemSo").innerHTML = "Có " + countC + " số chẵn, " + countL + " số lẻ";
+ * 
+ * Khối 3
+ * nhập vào 1 2 3
+ * => Có 1 số chẵn,2 số lẻ
+ * 
+ * 
+ */
+
 function demSo() {
     var num1 = Number(document.getElementById("inNum1").value);
     var num2 = Number(document.getElementById("inNum2").value);
@@ -30,9 +109,7 @@ function demSo() {
     var countL = 0;
     var countC = 0;
 
-    if (num1 < 0 || num2 < 0 || num3 < 0 || num1 == "" || num2 == "" || num3 == "") {
-        alert("Nhập lại");
-    }
+    if (num1 < 0 || num2 < 0 || num3 < 0 || num1 == "" || num2 == "" || num3 == "") {alert("Nhập lại");}
     if (num1 % 2 == 0)
         countC++;
     else
@@ -48,6 +125,47 @@ function demSo() {
 
     document.getElementById("txtDemSo").innerHTML = "Có " + countC + " số chẵn, " + countL + " số lẻ";
 }
+
+/**
+ * Bai 4: kiểm tra tam giác
+ * 
+ * Khối 1
+ * canh 1 (c1)
+ * canh 2 (c2)
+ * canh 3 (c3)
+ * 
+ * Khối 2:
+ * b1: khai báo giá trị:
+ * (Để xét tam giác vuông)
+ * var a1 = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2)); 
+ * var a2 = Math.sqrt(Math.pow(c1, 2) + Math.pow(c3, 2));
+ * var a3 = Math.sqrt(Math.pow(c2, 2) + Math.pow(c3, 2));
+ * 
+ * b2: Lập điều kiện
+ * if (c1 <= 0 || c2 <= 0 || c3 <= 0) (trường hợp ko nhập hoặc nhập cạnh nhỏ hơn ko sex ko xuất hiện tam giác)
+        alert("Nhập lại");
+    else {
+        if (c1 == c2 && c2 == c3)
+            document.getElementById("txtTamGiac").innerHTML = "Hình tam giác đều";
+        else {
+            if (c1 == c2 || c2 == c3 || c1 == c3)
+                document.getElementById("txtTamGiac").innerHTML = "Hình tam giác cân";
+            else {
+                if (a1 == c3 || a2 == c2 || a3 == c1)
+                    document.getElementById("txtTamGiac").innerHTML = "Hình tam giác vuông";
+                else
+                    document.getElementById("txtTamGiac").innerHTML = "Một loại tam giác nào đó";
+ * 
+ * b3: xuất ra màn hình
+ * document.getElementById("txtTamGiac").innerHTML = ..... 
+ * 
+ * Khối 3:
+ * 
+ * nhập vào 3 4 5
+ * => Hình tam giác vuông
+ * 
+ */
+
 function ktTamGiac() {
     var c1 = Number(document.getElementById("inpC1").value);
     var c2 = Number(document.getElementById("inpC2").value);
@@ -73,3 +191,32 @@ function ktTamGiac() {
         }
     }
 }
+
+//chức năng button
+
+function bai1(){
+    document.getElementById("form__1").style.display= "block";
+    document.getElementById("form__2").style.display= "none";
+    document.getElementById("form__3").style.display= "none";
+    document.getElementById("form__4").style.display= "none";
+
+}
+function bai2(){
+    document.getElementById("form__1").style.display= "none";
+    document.getElementById("form__2").style.display= "block";
+    document.getElementById("form__3").style.display= "none";
+    document.getElementById("form__4").style.display= "none";
+}
+function bai3(){
+    document.getElementById("form__1").style.display= "none";
+    document.getElementById("form__2").style.display= "none";
+    document.getElementById("form__3").style.display= "block";
+    document.getElementById("form__4").style.display= "none";
+}
+function bai4(){
+    document.getElementById("form__1").style.display= "none";
+    document.getElementById("form__2").style.display= "none";
+    document.getElementById("form__3").style.display= "none";
+    document.getElementById("form__4").style.display= "block";
+}
+
